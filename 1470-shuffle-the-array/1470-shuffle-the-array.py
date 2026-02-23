873211215494b9684ae729496e353525b888c168
Jayakrishnan VR <jayakrishnanvrjk@gmail.com>
2026-02-23 12:01:48 +0530

    Time: 61 ms (63.82%), Space: 19.5 MB (14.1%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.py
+++ 1470-shuffle-the-array/1470-shuffle-the-array.py
@@ -1,8 +1,7 @@
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
-        sub_list = nums[n:]
         outArray = []
         for i in range(n):
                 outArray.append(nums[i])
-                outArray.append(sub_list[i])
+                outArray.append(nums[i+n])
         return outArray
