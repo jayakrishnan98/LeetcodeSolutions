4d5fd6cc9de515780a0702862fa14b16bd504137
Jayakrishnan VR <jayakrishnanvrjk@gmail.com>
2026-03-27 12:43:08 +0530

    Time: 19 ms (41.45%), Space: 21.8 MB (62.83%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.py
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.py
@@ -5,8 +5,7 @@ class Solution:
         for num in nums:
             if num == 1:
                 cur_max += 1
-                if cur_max > act_max:
-                    act_max = cur_max
+                act_max = max(act_max, cur_max)
             else:
                 cur_max = 0
         return act_max
