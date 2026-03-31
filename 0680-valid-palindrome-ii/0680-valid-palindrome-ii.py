2f85b7ebf0582dfddbab6f2d8380fbb909007074
Jayakrishnan VR <jayakrishnanvrjk@gmail.com>
2026-03-31 12:59:39 +0530

    Time: 44 ms (80.65%), Space: 19.5 MB (89.23%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -1,11 +1,12 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        def isPalindrome(l, r):
-            while l < r:
+        
+        def is_palindrome(l, r):
+            while l<r:
                 if s[l] != s[r]:
                     return False
-                l += 1
-                r -= 1
+                l+=1
+                r-=1
             return True
 
         left, right = 0, len(s) - 1
@@ -15,6 +16,6 @@ class Solution:
                 left += 1
                 right -= 1
             else:
-                return isPalindrome(left + 1, right) or isPalindrome(left, right - 1)
-
+                return is_palindrome(left + 1, right) or is_palindrome(left, right - 1)
+        
         return True
