1a6172521556f226dd43ba1a0b2dc0d20103b16c
Jayakrishnan VR <jayakrishnanvrjk@gmail.com>
2026-04-17 17:55:41 +0530

    Time: 39 ms (66.75%), Space: 19.6 MB (88.55%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -1,37 +1,30 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        res = []
-        if len(p) > len(s):
-            return res
-        
+        if len(s) < len(p):
+            return []
+
+        left, res, count = 0, [], len(p)
+
         freq = [0] * 26
-        
-        # Build frequency map for p
-        for c in p:
-            freq[ord(c) - ord('a')] += 1
-        
-        left = 0
-        count = len(p)
-        
+
+        for word in p:
+            freq[ord(word) - ord('a')] += 1
+
         for right in range(len(s)):
-            # If current char is needed, decrease count
+
             if freq[ord(s[right]) - ord('a')] > 0:
                 count -= 1
-            
-            # Decrease freq (we are using this char)
+
             freq[ord(s[right]) - ord('a')] -= 1
-            
-            # If window size exceeds p, move left
-            if right - left + 1 > len(p):
-                # If left char was useful, restore count
+
+            if (right - left +1) > len(p):
                 if freq[ord(s[left]) - ord('a')] >= 0:
-                    count += 1
-                
+                    count += 1    
                 freq[ord(s[left]) - ord('a')] += 1
                 left += 1
             
-            # If all chars matched
             if count == 0:
                 res.append(left)
-        
+
+
         return res
