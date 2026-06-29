0a078fcfdbe879363bf39b82376b33c47df12bc3
Jayakrishnan VR <jayakrishnanvrjk@gmail.com>
2026-06-29 23:02:35 +0530

    Time: 0 ms (100%), Space: 19.3 MB (58.44%) - LeetHub

---
+++ 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.py
@@ -0,0 +1,9 @@
+class Solution:
+    def numOfStrings(self, patterns: List[str], word: str) -> int:
+        count = 0
+
+        for item in patterns:
+            if item in word:
+                count += 1
+        
+        return count
