4059f324f0127133badfc0175ee751d45862943c
Jayakrishnan VR <jayakrishnanvrjk@gmail.com>
2026-07-17 12:13:31 +0530

    Time: 0 ms (100%), Space: 19.3 MB (86.7%) - LeetHub

---
+++ 0046-permutations/0046-permutations.py
@@ -0,0 +1,23 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+
+        def backtrack(current):
+            # A complete permutation is formed
+            if len(current) == len(nums):
+                result.append(current.copy())
+                return
+
+            for num in nums:
+                # Do not use the same number twice
+                if num not in current:
+                    # Choose
+                    current.append(num)
+                    # Explore
+                    backtrack(current)
+
+                    # Undo the choice
+                    current.pop()
+
+        result = []
+        backtrack([])
+        return result
