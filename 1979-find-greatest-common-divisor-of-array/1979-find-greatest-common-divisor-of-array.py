126a73660e5df131139db052b432430c65fae1c0
Jayakrishnan VR <jayakrishnanvrjk@gmail.com>
2026-07-18 13:32:37 +0530

    Time: 0 ms (100%), Space: 19.4 MB (42.65%) - LeetHub

--- 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.py
+++ 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.py
@@ -1,11 +1,7 @@
 class Solution:
     def findGCD(self, nums: List[int]) -> int:
-        nums.sort()
-        a = nums[0]
-        b = nums[len(nums)-1]
-        print(nums)
-        print(a)
-        print(b)
+        a = min(nums)
+        b = max(nums)
         for i in range(a, 0, -1):
             if a % i == 0 and b % i == 0:
                 return i
