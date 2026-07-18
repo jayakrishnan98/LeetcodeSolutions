74739122c164305c30a91abe09806bd142f8ae9d
Jayakrishnan VR <jayakrishnanvrjk@gmail.com>
2026-07-19 05:12:57 +0530

    Time: 63 ms (96.8%), Space: 61.7 MB (6.4%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,22 +1,20 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         result = []
+
         def backtrack(start, current):
             if len(current) == k:
                 result.append(current.copy())
                 return
-            
-            for i in range(start, n+1):
 
+            numbers_needed = k - len(current)
+            last_possible_start = n - numbers_needed + 1
+
+            for i in range(start, last_possible_start + 1):
                 current.append(i)
-
-                backtrack(i+1, current)
-
+                backtrack(i + 1, current)
                 current.pop()
 
         backtrack(1, [])
+
         return result
-
-
-
-
