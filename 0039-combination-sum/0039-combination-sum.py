275fbccf5ee9d7ffc9fe2b0fdf4db5d49c7ea614
Jayakrishnan VR <jayakrishnanvrjk@gmail.com>
2026-07-25 19:26:43 +0530

    Time: 3 ms (95.66%), Space: 19.5 MB (64.3%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,6 +1,7 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         result = []
+        candidates.sort()
         def backtrack(start, current, remaining):
 
             if remaining == 0:
@@ -11,6 +12,8 @@ class Solution:
                 return 
 
             for index in range(start, len(candidates)):
+                if candidates[index] > remaining:
+                    break
                 item = candidates[index]
                 current.append(item)
                 backtrack(index, current, remaining-item)
