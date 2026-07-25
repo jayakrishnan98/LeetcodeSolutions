0f45bdf5356500d09fa02db18748584d7848bd77
Jayakrishnan VR <jayakrishnanvrjk@gmail.com>
2026-07-25 19:32:58 +0530

    Time: 7 ms (76.71%), Space: 19.6 MB (32.42%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -7,9 +7,6 @@ class Solution:
             if remaining == 0:
                 result.append(current.copy())
                 return
-                
-            elif remaining < 0:
-                return 
 
             for index in range(start, len(candidates)):
                 if candidates[index] > remaining:
