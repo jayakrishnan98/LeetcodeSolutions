daebfc6af96d4ed893c5eb02bdb91135919cf180
Jayakrishnan VR <jayakrishnanvrjk@gmail.com>
2026-07-26 06:11:13 +0530

    Time: 43 ms (62.01%), Space: 33.6 MB (65.92%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,22 +1,6 @@
-from typing import List
-
-
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
         result = []
-
-        def is_palindrome(text):
-            left = 0
-            right = len(text) - 1
-
-            while left < right:
-                if text[left] != text[right]:
-                    return False
-
-                left += 1
-                right -= 1
-
-            return True
 
         def backtrack(start, current):
             # We have partitioned the entire string
@@ -29,7 +13,7 @@ class Solution:
                 substring = s[start:end + 1]
 
                 # Only choose the substring if it is a palindrome
-                if is_palindrome(substring):
+                if substring == substring[::-1]:
                     current.append(substring)
 
                     # Partition the remaining part of the string
