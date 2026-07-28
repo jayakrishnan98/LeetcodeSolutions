9cdd1509d7bdac533b4b1f19d6fe2d5e866fc62c
Jayakrishnan VR <jayakrishnanvrjk@gmail.com>
2026-07-28 19:04:24 +0530

    Time: 297 ms (43.78%), Space: 21.9 MB (9.68%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.py
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.py
@@ -1,21 +1,9 @@
 class Solution:
     def smallestPalindrome(self, s: str) -> str:
-        frequency = [0] * 26
+        partition = len(s) // 2
 
-        for char in s:
-            frequency[ord(char) - ord("a")] += 1
+        base = sorted(s[:partition])
+        mid = [s[partition]] if len(s) % 2 == 1 else []
+        reversed_base = base[::-1]
 
-        left_parts = []
-        middle = ""
-
-        for index, count in enumerate(frequency):
-            char = chr(ord("a") + index)
-
-            left_parts.append(char * (count // 2))
-
-            if count % 2 == 1:
-                middle = char
-
-        left = "".join(left_parts)
-
-        return left + middle + left[::-1]
+        return "".join(base + mid + reversed_base)
