e290ab21723159256cf95672e3272f0e2a2a0fe7
Jayakrishnan VR <jayakrishnanvrjk@gmail.com>
2026-07-28 18:59:30 +0530

    Time: 306 ms (41.94%), Space: 20.7 MB (91.71%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.py
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.py
@@ -1,23 +1,23 @@
 class Solution:
     def smallestPalindrome(self, s: str) -> str:
-        if len(s) == 1:
-            return s
-        my_arr = [0] * 26
-        
-        for item in s:
-            my_arr[ord(item) - ord('a')] += 1
-        left = []
-        mid = ''
-        for i in range(len(my_arr)):
-            while my_arr[i] > 1:
-                left.append(chr(ord('a') + i))
-                my_arr[i] -= 2
-            if my_arr[i] == 1:
-                mid = chr(ord('a') + i)
-                my_arr[i] -= 1
-        left = "".join(left)
+        frequency = [0] * 26
 
-        result = left + mid + left[::-1]
+        for char in s:
+            frequency[ord(char) - ord("a")] += 1
 
-        return result
+        left_parts = []
+        middle = ""
 
+        for index, count in enumerate(frequency):
+            char = chr(ord("a") + index)
+
+            # Put half of this character's copies on the left.
+            left_parts.append(char * (count // 2))
+
+            # A palindrome can have one odd-frequency character.
+            if count % 2 == 1:
+                middle = char
+
+        left = "".join(left_parts)
+
+        return left + middle + left[::-1]
