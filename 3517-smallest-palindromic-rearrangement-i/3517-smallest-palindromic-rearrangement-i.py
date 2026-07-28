22e89803ba1bcc1f61812455e169f56f428467a6
Jayakrishnan VR <jayakrishnanvrjk@gmail.com>
2026-07-28 19:15:51 +0530

    Time: 263 ms (53%), Space: 22.1 MB (6.91%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.py
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.py
@@ -3,7 +3,9 @@ class Solution:
         partition = len(s) // 2
 
         base = sorted(s[:partition])
+
         mid = [s[partition]] if len(s) % 2 == 1 else []
+
         reversed_base = base[::-1]
 
         return "".join(base + mid + reversed_base)
