b1c5a2250af67c9f2d54396eddf0188ce4445d75
Jayakrishnan VR <jayakrishnanvrjk@gmail.com>
2026-08-14 12:33:47 +0530

    Time: 7 ms (29.86%), Space: 19.2 MB (60.1%) - LeetHub

--- 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.py
+++ 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.py
@@ -4,9 +4,9 @@ class Solution:
         result = 0
         start = -1
 
-        for end in range(len(s)):
-            freq[s[end]] += 1
-            while freq[s[end]] > 2:
+        for end, char in enumerate(s):
+            freq[char] += 1
+            while freq[char] > 2:
                 start += 1
                 freq[s[start]] -= 1
             result = max(result, end-start)
