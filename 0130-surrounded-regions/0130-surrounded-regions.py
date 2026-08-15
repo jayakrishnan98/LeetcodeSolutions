bc9b456100451878c19e709ed73df94d09b9189b
Jayakrishnan VR <jayakrishnanvrjk@gmail.com>
2026-08-15 16:04:41 +0530

    Time: 3 ms (83.31%), Space: 22.5 MB (40.64%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -1,29 +1,39 @@
 class Solution:
     def solve(self, board: List[List[str]]) -> None:
         ROWS, COLS = len(board), len(board[0])
-        def capture(r,c):
-            if (r<0 or c<0 or r==ROWS or c==COLS or board[r][c] != "O"):
-                return
-            board[r][c] = "T"
-            capture(r+1, c)
-            capture(r-1, c)
-            capture(r, c+1)
-            capture(r, c-1)
-        
-        # 1. (DFS) Capture unsurrounded regions (O -> T)
-        for r in range(ROWS):
-            for c in range(COLS):
-                if (board[r][c] == "O" and (r in [0, ROWS-1] or c in [0, COLS - 1])):
-                    capture(r, c)
 
-        # 2. Capture surrounded regions (O -> X)
+        def capture(r, c):
+            if (
+                r < 0 or r >= ROWS or
+                c < 0 or c >= COLS or
+                board[r][c] != "O"
+            ):
+                return
+
+            board[r][c] = "T"
+
+            capture(r + 1, c)
+            capture(r - 1, c)
+            capture(r, c + 1)
+            capture(r, c - 1)
+
+        # 1. Mark all boundary-connected O's as T
+        for r in range(ROWS):
+            capture(r, 0)
+            capture(r, COLS - 1)
+
+        for c in range(COLS):
+            capture(0, c)
+            capture(ROWS - 1, c)
+
+        # 2. Capture surrounded O's
         for r in range(ROWS):
             for c in range(COLS):
-                if board[r][c]== "O":
+                if board[r][c] == "O":
                     board[r][c] = "X"
 
-        # 3. Uncapture unsurrounded regions (T -> O)
+        # 3. Restore boundary-connected O's
         for r in range(ROWS):
             for c in range(COLS):
-                if board[r][c]== "T":
+                if board[r][c] == "T":
                     board[r][c] = "O"
