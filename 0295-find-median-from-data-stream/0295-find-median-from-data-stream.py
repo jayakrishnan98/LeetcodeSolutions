af6b8959cad52b45b592fc3c809bcf60e930c9cd
Jayakrishnan VR <jayakrishnanvrjk@gmail.com>
2026-09-21 15:38:34 +0530

    Time: 185 ms (50.73%), Space: 42.5 MB (23.98%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -1,19 +1,18 @@
 import heapq
 class MedianFinder:
     def __init__(self):
-        self.small = []   # max heap
-        self.large = []   # min heap
+        self.small, self.large  = [], []   # max heap
+        
     def addNum(self, num: int) -> None:
-        # Step 1: Put num into the left/smaller half
         heapq.heappush(self.small, -num)
-        # Step 2: Make sure every element in small <= every element in large
-        if self.large and (-self.small[0] > self.large[0]):
-            small_top = -heapq.heappop(self.small)
-            large_top = heapq.heappop(self.large)
 
-            heapq.heappush(self.small, -large_top)
-            heapq.heappush(self.large, small_top)
-        # Step 3: Balance the sizes
+        if len(self.large) and self.large[0] < -self.small[0]:
+            small_pop = -heapq.heappop(self.small)
+            large_pop = heapq.heappop(self.large)
+
+            heapq.heappush(self.small, -large_pop)
+            heapq.heappush(self.large, small_pop)
+        
         if len(self.small) > len(self.large) + 1:
             num = -heapq.heappop(self.small)
             heapq.heappush(self.large, num)
@@ -22,7 +21,9 @@ class MedianFinder:
             num = heapq.heappop(self.large)
             heapq.heappush(self.small, -num)
 
+
     def findMedian(self) -> float:
         if len(self.small) > len(self.large):
             return -self.small[0]
-        return (-self.small[0] + self.large[0]) / 2
+        else:
+            return (-self.small[0] + self.large[0]) / 2
