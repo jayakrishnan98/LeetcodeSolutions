5124d5ff28703d7afe452b8b8fb4397b954f0e0b
Jayakrishnan VR <jayakrishnanvrjk@gmail.com>
2026-09-25 15:03:49 +0530

    Time: 3 ms (32.57%), Space: 19.6 MB (36.89%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,22 +1,18 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
 
+
         def backtrack(current):
-            # A complete permutation is formed
-            if len(current) == len(nums):
+            if len(nums) == len(current):
                 result.append(current.copy())
                 return
-
+            
             for num in nums:
-                # Do not use the same number twice
                 if num not in current:
-                    # Choose
                     current.append(num)
-                    # Explore
                     backtrack(current)
-                    # Undo the choice
                     current.pop()
-
+        
         result = []
         backtrack([])
         return result
